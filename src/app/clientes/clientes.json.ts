dfc75a034352e95619d559c64fb501f51f296788
Angular.David <davcas@hotmail.com>
2022-07-21 23:03:16 -0500

    casa

---
+++ src/app/clientes/clientes.json.ts
@@ -0,0 +1,9 @@
+import { Cliente } from './cliente';
+
+export const CLIENTES: Cliente[] = [
+  {id: 1, nombre: 'Leticia', apellido: 'Cuaya', email: 'leticiac@mail.com', createAt: '2022-07-11'},
+  {id: 2, nombre: 'Mr. Jhon', apellido: 'Doe', email: 'john.doe@mail.com', createAt: '2022-07-11'},
+  {id: 3, nombre: 'Linus', apellido: 'Torvlas', email: 'linust@mail.com', createAt: '2022-07-11'},
+  {id: 4, nombre: 'Rasmus', apellido: 'Lerdor', email: 'rasmusl@mail.com', createAt: '2022-07-11'},
+  {id: 5, nombre: 'Erich', apellido: 'Gamma', email: 'erichg@mail.com', createAt: '2022-07-11'},
+]
