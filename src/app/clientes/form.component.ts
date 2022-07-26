b88c5724b510f8e0852885c8228a3261b3c87fe9
DavidCasSseg <109768908+DavidCasSseg@users.noreply.github.com>
2022-07-26 17:57:52 -0500

    commit8

--- src/app/clientes/form.component.ts
+++ src/app/clientes/form.component.ts
@@ -1,21 +1,25 @@
 import { Component, OnInit } from '@angular/core';
-import { Cliente } from './cliente'
+import { Cliente } from './cliente';
+import { ClienteService } from './cliente.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-form',
   templateUrl: './form.component.html'
 })
 export class FormComponent implements OnInit {
+
   public cliente: Cliente = new Cliente();
   public titulo: string = "Crear Cliente"
 
-  constructor() { }
+  constructor(public clienteService: ClienteService, public router: Router) { }
 
   ngOnInit(): void {
   }
 
   public create(): void{
-    console.log("Clicked!")
-    console.log(this.cliente)
+    this.clienteService.create(this.cliente).subscribe(
+      response => this.router.navigate(['/clientes'])
+    )
   }
 }
