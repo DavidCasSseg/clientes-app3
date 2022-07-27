11ee67c364ef24a69267662193418fac3c6d3893
DavidCasSseg <109768908+DavidCasSseg@users.noreply.github.com>
2022-07-27 18:00:03 -0500

    Escribiendo update en cliente.service.ts y en form.component.ts

--- src/app/clientes/form.component.ts
+++ src/app/clientes/form.component.ts
@@ -1,7 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { Cliente } from './cliente';
 import { ClienteService } from './cliente.service';
-import { Router } from '@angular/router';
+import { Router, ActivatedRoute } from '@angular/router';
+import swal from 'sweetalert2';
 
 @Component({
   selector: 'app-form',
@@ -12,14 +13,36 @@ export class FormComponent implements OnInit {
   public cliente: Cliente = new Cliente();
   public titulo: string = "Crear Cliente"
 
-  constructor(public clienteService: ClienteService, public router: Router) { }
+  constructor(public clienteService: ClienteService,
+    public router: Router,
+    public activatedRoute: ActivatedRoute) { }
 
   ngOnInit(): void {
+    this.cargarCliente()
+  }
+
+  cargarCliente(): void{
+    this.activatedRoute.params.subscribe(params => {
+      let id = params['id']
+      if(id){
+        this.clienteService.getCliente(id).subscribe((cliente) => this.cliente = cliente)
+      }
+    })
   }
 
   public create(): void{
     this.clienteService.create(this.cliente).subscribe(
-      response => this.router.navigate(['/clientes'])
-    )
+      cliente => {
+        this.router.navigate(['/clientes'])
+        swal.fire('Nuevo cliente', `Cliente ${cliente.nombre} creado con exito!`, 'success')
+      })
+  }
+
+  update(): void{
+    this.clienteService.update(this.cliente)
+    .subscribe( cliente => {
+      this.router.navigate(['/clientes'])
+      swal.fire('Cliente Actualizado',  `Cliente ${cliente.nombre} actualizado con exito!`, 'success')
+    })
   }
 }
